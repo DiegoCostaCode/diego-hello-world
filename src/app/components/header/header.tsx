3789a9c25e1be3ad47cef8be5459f0b1961a6b03
Diego Costa <diegocosta.contato2021@gmail.com>
2025-01-17 23:31:14 -0300

    [feat]-Hello World

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+            <div>
+                <h1>Bem-vindo à Página Inicial!</h1>
+                <p>Esse é um exemplo de componente no Next.js.</p>
+            </div>
+      </>
+       )
+}
